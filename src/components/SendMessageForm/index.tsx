4c824650bbf03a061aa4c185e9562200815ea34d
Higor Allan <higor.allan21@gmail.com>
2021-10-23 09:07:26 -0300

    APlicacao Front-end da NLW

---
+++ src/components/SendMessageForm/index.tsx
@@ -0,0 +1,57 @@
+import { useContext, useState, FormEvent } from 'react'
+import { VscGithubInverted, VscSignOut } from 'react-icons/vsc'
+import styles from './styles.module.scss'
+import { AuthContext } from '../../contexts/auth';
+import { api } from '../../services/api';
+
+
+export function SendMessageForm(){
+    const { user, signOut } = useContext(AuthContext)
+    const [message,setMessage]=useState('')
+
+
+    const handleSendMessage = async (e: FormEvent)=>{
+        e.preventDefault();
+
+        if(!message.trim()){
+            return;
+        }
+
+        await api.post('messages', {message} )
+
+        setMessage('')
+    }
+
+    return(
+        <div className={styles.sendMessageFormWrapper}>
+            <button className={styles.signOutButton} onClick={signOut}>
+                <VscSignOut size="32"/>
+            </button>
+
+            <header className={styles.userInformation}>
+                <div className={styles.userImage}>
+                    <img src={user?.avatar_url} alt={user?.name}/>
+                </div>
+                <strong className={styles.userName}>{user?.name}</strong>
+                <span className={styles.userGithub}>
+                    <VscGithubInverted size='16px'/>
+                    {user?.login}
+                </span>
+            </header>
+
+            <form className={styles.sendMessageForm} onSubmit={handleSendMessage}>
+                <label htmlFor="message">Mensagem</label>
+
+                <textarea
+                    name="message"
+                    id='message'
+                    placeholder='Qual sua expectative para o evento?'
+                    value={message}
+                    onChange={(e)=>setMessage(e.target.value)}
+                />
+
+                <button type='submit'>Enviar Mensagem</button>
+            </form>
+        </div>
+    )
+}
